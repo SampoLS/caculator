b7e7298f3a3f0ca9c836b741d455c47d218f0df8
SampoLS <mr.sampo@outlook.com>
2022-06-09 23:25:03 +0800

    update

--- index.js
+++ index.js
@@ -1,125 +1,115 @@
-const root = document.getElementById("root")
+const root = document.getElementById("root");
 
-const cellWidth = 50
-const cellHeight = 50
+const cellWidth = 50;
+const cellHeight = 50;
 
-let topShowBar, bottomShowBar
-let storedValue
+let topShowBar, bottomShowBar;
+let storedValue;
 
-let clickEl
+let clickEl;
 
-const renderResult = () => {
-  const strArr = ['section', 'div', 'div']
-  const elsArr = []
+const main = () => {
+  renderResult();
+  renderCells();
+};
+
+main();
+
+function renderResult() {
+  const strArr = ["section", "div", "div"];
+  const elsArr = [];
 
   for (let i = 0; i < strArr.length; i++) {
-    elsArr[i] = document.createElement(strArr[i])
+    elsArr[i] = document.createElement(strArr[i]);
     if (i !== 0) {
-      elsArr[0].appendChild(elsArr[i])
-      elsArr[i].className = 'bar' + i
+      elsArr[0].appendChild(elsArr[i]);
+      elsArr[i].className = "bar" + i;
     }
   }
 
-  const [section, topBar, bottomBar] = elsArr
-  root.appendChild(section)
+  const [section, topBar, bottomBar] = elsArr;
+  root.appendChild(section);
 
-  topShowBar = topBar
-  bottomShowBar = bottomBar
+  topShowBar = topBar;
+  bottomShowBar = bottomBar;
 
-  return [topBar, bottomBar]
-}
+  return [topBar, bottomBar];
+};
 
-const createCell = (grid, isDouble, text) => {
-  const cell = document.createElement('button')
+function renderCells() {
+  const grid = document.createElement("section");
+  grid.className = "grid";
 
-  if (isDouble) {
-    cell.style.width = cellWidth * 2 + 'px'
-  } else {
-    cell.style.width = cellWidth + 'px'
-  }
-
-  cell.style.height = cellHeight + 'px'
-  cell.textContent = text
-  grid.appendChild(cell)
-
-  if (typeof text === 'number') {
-    cell.addEventListener('click', () => {
-      bottomShowBar.textContent += text
-    })
-  }
-  if (cell.textContent === '÷') {
-    cell.addEventListener('click', () => {
-      storedValue = bottomShowBar.textContent
-      topShowBar.textContent =  storedValue + '÷'
-      bottomShowBar.textContent = ''
-      clickEl = cell
-    })
-  } else if (cell.textContent === '*') {
-    cell.addEventListener('click', () => {
-      storedValue = bottomShowBar.textContent
-      topShowBar.textContent = storedValue + '*'
-      bottomShowBar.textContent = ''
-      clickEl = cell
-    })
-  } else if (cell.textContent === '+') {
-    cell.addEventListener('click', () => {
-      storedValue = bottomShowBar.textContent
-      topShowBar.textContent = storedValue + '+'
-      bottomShowBar.textContent = ''
-      clickEl = cell
-    })
-  } else if (cell.textContent === '-') {
-    cell.addEventListener('click', () => {
-      storedValue = bottomShowBar.textContent
-      topShowBar.textContent = storedValue + '-'
-      bottomShowBar.textContent = ''
-      clickEl = cell
-    })
-  } else if (cell.textContent === '=') {
-      cell.addEventListener('click', () => {
-      if (clickEl.textContent === '÷') {
-        const res = Number(storedValue) / Number(bottomShowBar.textContent)
-        topShowBar.textContent = ''
-        bottomShowBar.textContent = res
-      } else if (clickEl.textContent === '*') {
-        const res = Number(storedValue) * Number(bottomShowBar.textContent)
-        topShowBar.textContent = ''
-        bottomShowBar.textContent = res
-      } else if (clickEl.textContent === '+') {
-        const res = Number(storedValue) + Number(bottomShowBar.textContent)
-        topShowBar.textContent = ''
-        bottomShowBar.textContent = res
-      } else if (clickEl.textContent === '-') {
-        const res = Number(storedValue) - Number(bottomShowBar.textContent)
-        topShowBar.textContent = ''
-        bottomShowBar.textContent = res
-      }
-    })
-  } 
-}
-
-const renderCells = () => {
-  const grid = document.createElement('section')
-  grid.className = 'grid'
-
-  const contents = ['AC', 'DEL', '÷', 1, 2, 3, '*', 4, 5, 6, '+', 7, 8, 9, '-', '.', 0, '=']
+  const contents = ["AC", "DEL", "÷", 1, 2, 3, "*", 4, 5, 6, "+", 7, 8, 9, "-", ".", 0, "="];
 
   for (let i = 0; i < contents.length; i++) {
     if (i === 0) {
-      createCell(grid, true, contents[i])
+      createCell(grid, true, contents[i]);
     } else if (i === contents.length - 1) {
-      createCell(grid, true, contents[i])
+      createCell(grid, true, contents[i]);
     } else {
-      createCell(grid, false, contents[i])
+      createCell(grid, false, contents[i]);
     }
   }
 
-  root.appendChild(grid)
-}
+  root.appendChild(grid);
+};
 
-const main = () => {
-  renderResult()
-  renderCells()
-}
+function createCell(grid, isDouble, text) {
+  const cell = document.createElement("button");
 
-main()
+  if (isDouble) {
+    cell.style.width = cellWidth * 2 + "px";
+  } else {
+    cell.style.width = cellWidth + "px";
+  }
+
+  cell.style.height = cellHeight + "px";
+  cell.textContent = text;
+  grid.appendChild(cell);
+
+  cell.addEventListener("click", () => {
+    if (typeof text === "number") {
+      bottomShowBar.textContent += text;
+    }
+    if (cell.textContent === "÷") {
+      storedValue = bottomShowBar.textContent;
+      topShowBar.textContent = storedValue + "÷";
+      bottomShowBar.textContent = "";
+      clickEl = cell;
+    } else if (cell.textContent === "*") {
+      storedValue = bottomShowBar.textContent;
+      topShowBar.textContent = storedValue + "*";
+      bottomShowBar.textContent = "";
+      clickEl = cell;
+    } else if (cell.textContent === "+") {
+      storedValue = bottomShowBar.textContent;
+      topShowBar.textContent = storedValue + "+";
+      bottomShowBar.textContent = "";
+      clickEl = cell;
+    } else if (cell.textContent === "-") {
+      storedValue = bottomShowBar.textContent;
+      topShowBar.textContent = storedValue + "-";
+      bottomShowBar.textContent = "";
+      clickEl = cell;
+    } else if (cell.textContent === "=") {
+      if (clickEl.textContent === "÷") {
+        const res = Number(storedValue) / Number(bottomShowBar.textContent);
+        topShowBar.textContent = "";
+        bottomShowBar.textContent = res;
+      } else if (clickEl.textContent === "*") {
+        const res = Number(storedValue) * Number(bottomShowBar.textContent);
+        topShowBar.textContent = "";
+        bottomShowBar.textContent = res;
+      } else if (clickEl.textContent === "+") {
+        const res = Number(storedValue) + Number(bottomShowBar.textContent);
+        topShowBar.textContent = "";
+        bottomShowBar.textContent = res;
+      } else if (clickEl.textContent === "-") {
+        const res = Number(storedValue) - Number(bottomShowBar.textContent);
+        topShowBar.textContent = "";
+        bottomShowBar.textContent = res;
+      }
+    }
+  });
+};
